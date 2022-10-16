1dbddb3e3e3858bb23c2c4b56a17b21eb842ae5b
Frank Palazzolo <frank@avoidspikes.com>
2022-10-16 00:36:11 -0400

    Move old re code into orig directory
    Update asm file to build with TASM - images match!

--- software/dumper.asm
+++ software/dumper.asm
@@ -2,7 +2,9 @@
 ; Original Code and hardware by Frank Palazzolo
 ; Code written and working sometime in the early 2000's
 ; Source Reverse-engineered from Binary(!) 2022-October
-; (This code has not been re-assembled, yet)
+;
+; This code can be built with TASM Version 3.2:
+;   tasm -70 -g3 -f00 -c dumper.asm dumper.bin
 ;
 ; Dumper code for:
 ;       PIC7020, TMS7020 - 2K bytes ROM, 128 bytes RAM
@@ -38,80 +40,97 @@
 ; If chip only has 2K internal ROM, it will be in the last 2K of the data
 ;
 
-        .ORG    0F000H
+R0      .equ    0
+R2      .equ    2
+R4      .equ    4
+R5      .equ    5
+R6      .equ    6
+R7      .equ    7
+R8      .equ    8
+R9      .equ    9
+R10     .equ    10
+
+BPORT   .equ    6
+
+RAMDEST .equ    $002C       ; location in ram for the code to be copied into and run from
+
+        .ORG    $E000       ; building 8K image for original 8K battery-backed SRAM design
+        .BYTE   00          ; need this, or we don't get an 8K image
+
+        .ORG    $F000
 
         ; Initialize port, stack
 START:
-        MOVP    %>FF, P6    ; PORTB = FF, PORTB.0=1 (MC high, make sure external ROM enabled)
-        MOVP    %>0C, B     ; B  = 0C
+        MOVP    %$FF,BPORT  ; PORTB = FF, PORTB.0=1 (MC high, make sure external ROM enabled)
+        MOV     %$0C,B      ; B  = 0C
         LDSP                ; SP = 000C - we don't use stack anyway
 
         ; Copy code from External ROM to RAM
 
-        MOVD    RAMCODE,R5  ; R5 = F030
-        MOVD    RAMDEST,R7  ; R7 = 002C
+        MOVD    %RAMCODE,R5 ; R5 = F030
+        MOVD    %RAMDEST,R7 ; R7 = 002C
 CPYLOOP:
-        LDA     (R5)        ; grab code byte
-        STA     (R7)        ; move code byte
+        LDA     *R5         ; grab code byte
+        STA     *R7         ; move code byte
         INC     R7          ; increment destination
-        ADC     %>0,R6      ; ripple carry to high byte
+        ADC     %$0,R6      ; ripple carry to high byte
         INC     R5          ; increment source
-        ADC     %>0,R4      ; ripple carry to high byte
-        CMP     %>F0,R4     ; am I at high byte of ENDCODE?
-        JNZ     LOOP        ; if we are not done, loop to copy next byte
-        CMP     %>83,R5     ; am I at low byte of ENDCODE?
+        ADC     %$0,R4      ; ripple carry to high byte
+        CMP     %$F0,R4     ; am I at high byte of ENDCODE?
+        JNZ     CPYLOOP     ; if we are not done, loop to copy next byte
+        CMP     %$83,R5     ; am I at low byte of ENDCODE?
         JNZ     CPYLOOP     ; if we are not done, loop to copy next byte
 
         ; We reached ENDCODE, so our code copy to RAM is done
 
-        MOVD    %>F100,R7   ; R7 = F100, location to try to write total number of bytes to
-        MOV     %>1,R10     ; R10 = 1, Times to do the dump
+        MOVD    %$F100,R7   ; R7 = F100, location to try to write total number of bytes to
+        MOV     %$1,R10     ; R10 = 1, Times to do the dump
 
-        BR      RAMDEST     ; jump to code in RAM
+        BR      @RAMDEST    ; jump to code in RAM
 
 ; Start of code to be executed in RAM
 ; (Must be relocatable)
 
 RAMCODE:
-        MOVP    %>FE,P6     ; PORTB = FE, PORTB.0=0 (Flip the MC pin low, internal ROM enabled)
-        MOV     %>FF,B      ; B = FF
+        MOVP    %$FE,BPORT  ; PORTB = FE, PORTB.0=0 (Flip the MC pin low, internal ROM enabled)
+        MOV     %$FF,B      ; B = FF
 DELAY:
         DEC     B           ; B = B-1
         JNZ     DELAY       ; wait a bit for things to settle
 DODUMP:
-        MOVD    %>F000,R5   ; R5 = F000, (Start of Internal ROM)
-        MOVD    %>0000,R9   ; R9 = 0000, byte counter
+        MOVD    %$F000,R5   ; R5 = F000, (Start of Internal ROM)
+        MOVD    %$0000,R9   ; R9 = 0000, byte counter
 NXTBYTE:
-        LDA     (R5)        ; Read internal ROM byte
+        LDA     *R5         ; Read internal ROM byte
         ADD     R0,R9       ; Add 1 to byte counter
-        ADC     %>0,R8      ; ripple carry to high byte of byte counter
-        MOV     %>8,R2      ; R2 = 8, bit counter?
+        ADC     %$0,R8      ; ripple carry to high byte of byte counter
+        MOV     %$8,R2      ; R2 = 8, bit counter?
 NXTBIT:
         RR      A           ; get each bit, LSB first
         JHS     DOONE       ; if 1, jump
-        ANDP    %>FD,P6     ; PORTB.1 = 0
+        ANDP    %$FD,BPORT  ; PORTB.1 = 0
         JMP     AHEAD
 DOONE:
-        ORP     %>02,P6     ; PORTB.1 = 1
+        ORP     %$02,BPORT  ; PORTB.1 = 1
 AHEAD:
 
-        MOV     %>10,B      ; small delay
+        MOV     %$10,B      ; small delay
 SDELAY:
         DEC     B
         JNZ     SDELAY
 
-        ANDP    %>FB,P6     ; PORTB.2 = 0
+        ANDP    %$FB,BPORT  ; PORTB.2 = 0
 
-        MOV     %>10,B      ; small delay
+        MOV     %$10,B      ; small delay
 SDELAY1:
         DEC     B
         JNZ     SDELAY1
 
-        ORP     %>04,P6     ; PORTB.2 = 1
+        ORP     %$04,BPORT  ; PORTB.2 = 1
         DEC     R2          ; decrement bit counter
         JNZ     NXTBIT      ; do next bit
         INC     R5          ; increment for next location to read
-        ADC     %>0,R4
+        ADC     %$0,R4
         JNZ     NXTBYTE     ; unless we roll over to 0000, do next byte
 
         ; Try to write the total number of bytes read (2 bytes) on this pass
@@ -119,10 +138,10 @@ SDELAY1:
         ; (those addresses are ROM)
 
         MOV     R8,A
-        STA     (R7)
+        STA     *R7
         INC     R7
         MOV     R9,A
-        STA     (R7)
+        STA     *R7
         INC     R7
 
         DEC     R10         ; Do it R10 times
@@ -137,7 +156,13 @@ ENDCODE:
 
         .ORG    0FFF8H
 
-        .DEFW   START
-        .DEFW   START
-        .DEFW   START
+        .MSFIRST            ; need this, or .word statements are backwards for this CPU
+        
+        .word   START
+        .word   START
+        .word   START
+        .word   START
+
+        .end
+
 
