d29c4fdfc4df96f4bedd4ba01a09141db1b88718
Frank Palazzolo <frank@avoidspikes.com>
2022-10-21 20:31:08 -0400

    Add stub for Arduino code

---
+++ software/dumper/dumper.ino
@@ -0,0 +1,20 @@
+
+// Dumper
+//
+// Connections:
+
+// Arduino - TMS7000   - DIP PIN
+// D5      - CLKIN     - PIN 17
+// D7      - ~RESET    - PIN 14 
+// D10     - SS / B3   - PIN 37
+// D11     - DATA / B1 - PIN 4
+// D13     - CLK / B2  - PIN 5
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
